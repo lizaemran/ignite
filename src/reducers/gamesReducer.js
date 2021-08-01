2da451887ecf2a7f268c69da5b0de2473b36303c
lizaemran <lizaemran56@gmail.com>
2021-08-01 15:58:16 +0500

    5. Fetching First Data

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -6,19 +6,12 @@ const initState = {
 const gamesReducer = (state= initState, action) => {
 switch (action.type){
     case "FETCH_GAMES":
-        return {...state}
+        return {...state, popular: action.payload.popular }
     default: 
         return {...state}
 }
 };
-//Action Creator
-const fetchGames = () => {
-    return {
-        type: "FETCH_GAMES",
-    };
-};
 
-fetchGames();
 
 
 export default gamesReducer;
