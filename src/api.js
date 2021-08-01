3d5efe5ff3fc7a3436feb7d4ff2653ce273dc061
lizaemran <lizaemran56@gmail.com>
2021-08-01 16:43:21 +0500

    6. Fetching Other Games

--- src/api.js
+++ src/api.js
@@ -26,7 +26,10 @@ const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
-
+const upcoming_games = `&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
 const popular_games = `&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const new_games = `&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${new_games}`;
