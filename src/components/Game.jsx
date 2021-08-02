0e529938e343c310e207584d197cc0dfbbb1ba3d
lizaemran <lizaemran56@gmail.com>
2021-08-02 15:05:13 +0500

    15. Toggling Components

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -7,8 +7,8 @@ import {Link} from 'react-router-dom';
 const Game = ({name, released, image, id}) => {
     const dispatch = useDispatch();
     const loadDetailHandler = () => {
+        document.body.style.overflow = "hidden";
         dispatch(loadDetail(id));
-        console.log(id);
     }
     return (
         <StyledGame onClick = {loadDetailHandler}>
@@ -27,6 +27,7 @@ const StyledGame = styled(motion.div)`
     text-align: center;
     border-radius: 1rem;
     cursor: pointer;
+    overflow: hidden;
     img{
         width: 100%;
         height: 40vh;
--- src/components/GlobalStyles.jsx
+++ src/components/GlobalStyles.jsx
@@ -37,5 +37,8 @@ const GlobalStyles = createGlobalStyle`
         text-decoration: none;
         color: #333;
     }
+    img{
+        display: block;
+    }
 `;
 export default GlobalStyles;
